874609244f7744fc3ed7639cc264639a2053a0d9
Unknown <cap.noer@gmail.com>
2018-06-12 13:26:59 +0200

    Hey, LETS GOOO!

---
+++ pre-app-code-project/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  pre-app-code-project
+//
+//  Created by Bruger on 12/06/2018.
+//  Copyright © 2018 Mercantec. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+walkdmadsjdka
