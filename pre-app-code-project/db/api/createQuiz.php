e0423a097470d404af0bcea00030fee267dee2dd
Unknown <benjaminsandland@gmail.com>
2018-06-14 14:25:09 +0200

    Added API for database
    API with functionalities such as;
    - Create user
    - Create quiz with questions and correct answers
    - Insert answers from users

---
+++ pre-app-code-project/db/api/createQuiz.php
@@ -0,0 +1,46 @@
+<?php
+	$response = array();
+
+	$example = "http://localhost:8080/dbConn/api/createQuiz.php?title=quiz1&description=hej&creator=2";
+
+	if($_SERVER['REQUEST_METHOD']=='GET'){
+ 
+	    //getting values
+	    $title = $_GET['title'];
+	    $description = $_GET['description'];	
+	    $creator = $_GET['creator'];
+	 
+	    //including the db operation file
+	    require_once '../includes/dbOperation.php';
+	 
+	    $db = new dbOperation();
+		if(isset($_GET["title"], $_GET["description"], $_GET["creator"]))
+		{
+		    //inserting values 
+			if($db->createQuiz($title, $description, $creator)){
+			    $response['error']=false;
+			    $response['message']='Quiz added successfully';
+			} 
+			else {
+			    $response['error']=true;
+			    $response['message']='Could not add quiz';
+			}
+
+		}
+		else {
+			$response['error']=true;
+			$response['message']='You are not authorized';
+		}
+	}
+	else {
+		$title = null;
+		$description = null;
+		$creator = null;
+
+		echo "Parameters have not been set!";
+	}
+
+
+	echo json_encode($response);
+
+?>
---
+++ pre-app-code-project/db/api/createUser.php
@@ -0,0 +1,49 @@
+<?php
+	$response = array();
+
+	$example = "http://localhost:8080/dbConn/api/createUser.php?fname=ben&midname=ja&lname=min&uname=bensand&pass=1234&mail=hej@meddig.dk";
+
+	if($_SERVER['REQUEST_METHOD']=='GET'){
+ 
+	    //getting values
+	    $firstname = $_GET['fname'];
+	    $middlename = $_GET['midname'];
+	    $lastname = $_GET['lname'];
+	    $username = $_GET['uname'];
+	    $password = $_GET['pass'];
+	    $email = $_GET['mail'];
+	 
+	    //including the db operation file
+	    require_once '../includes/dbOperation.php';
+	 
+	    $db = new dbOperation();
+		if(isset($_GET["fname"], $_GET["midname"], $_GET["lname"], $_GET["uname"], $_GET["pass"], $_GET["mail"]))
+		{
+		    //inserting values 
+			if($db->createUser($firstname, $middlename, $lastname, $username, $password, $email)){
+			    $response['error']=false;
+			    $response['message']='User added successfully';
+			} 
+			else {
+			    $response['error']=true;
+			    $response['message']='Could not add user';
+			}
+		}
+		else {
+			$response['error']=true;
+			$response['message']='You are not authorized';
+		}
+	}
+	else {
+		$firstname = null;
+		$middlename = null;
+		$lastname = null;
+		$username = null;
+		$password = null;
+		$email = null;
+
+		echo "Parameters have not been set!";
+	}
+
+	echo json_encode($response);
+?>
